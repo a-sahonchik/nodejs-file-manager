1c72f61c5132cf5b1979f3f2e95460de1d168012
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-18 21:07:48 +0300

    feat: add regex to parse files and folders with space in name

--- src/commands/parseCommands.js
+++ src/commands/parseCommands.js
@@ -1,9 +1,16 @@
 const parseCommandFromInput = async (command) => {
-    const splittedCommand = command.split(' ');
+    const regex = new RegExp(
+        '(([^"\\s]*"[^"]+"[^"\\s]*)+|([^\'\\s]*\'[^\']+\'[^\'\\s]*)+|[^\\s"]+)',
+        'gmi'
+    );
+
+    const quotesRegex = /(["'])/gmi;
+
+    const matches = command.match(regex).map(match => match.replaceAll(quotesRegex, ''));
 
     const parsedCommand = {
-        name: splittedCommand[0].toString(),
-        arguments: splittedCommand.slice(1),
+        name: matches[0].toString(),
+        arguments: matches.slice(1),
     };
 
     return parsedCommand;

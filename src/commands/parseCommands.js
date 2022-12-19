f077bb15f0c5fb0b515467bdaf4e92885474aae5
Alexey Sahonchik <sahonchik.alexey@gmail.com>
2022-12-19 12:55:08 +0300

    refactor: refactor regexp for parsing commands

--- src/commands/parseCommands.js
+++ src/commands/parseCommands.js
@@ -1,12 +1,19 @@
+const REGEXP_FLAG_GMI = 'gmi';
+
 const parseCommandFromInput = async (command) => {
-    const regex = new RegExp(
+    const parseCommandArgumentsRegExp = new RegExp(
         '(([^"\\s]*"[^"]+"[^"\\s]*)+|([^\'\\s]*\'[^\']+\'[^\'\\s]*)+|[^\\s"]+)',
-        'gmi'
+        REGEXP_FLAG_GMI
     );
 
-    const quotesRegex = /(["'])/gmi;
+    const findQuotesRegExp = new RegExp(
+        '(["\'])',
+        REGEXP_FLAG_GMI
+    );
 
-    const matches = command.match(regex).map(match => match.replaceAll(quotesRegex, ''));
+    const matches = command
+        .match(parseCommandArgumentsRegExp)
+        .map(match => match.replaceAll(findQuotesRegExp, ''));
 
     const parsedCommand = {
         name: matches[0].toString(),
